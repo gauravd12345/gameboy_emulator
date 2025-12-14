54cb15154b0a974b7116c4c5970b0be8583a6fc2
Gaurav <gauravdharmadhikari2@gmail.com>
2025-12-14 03:21:09 -0800

    finished bootROM sequence

--- main.cpp
+++ main.cpp
@@ -2,27 +2,16 @@
 #include <fstream>
 #include "cpu.h"
 
-
-
-
 int main(){
     CPU cpu;
 
-    //cpu.printMemory();
     bool status = true;
-    while(status && cpu.reg.pc != 0xff){
+    while(status && cpu.reg.pc < 0x00FF){
         uint8_t instruction = cpu.fetch();
         status = cpu.execute(instruction);
 
     }
-    
+  
     cpu.printRegisters();
 
-    // uint8_t instruction = cpu.fetch();
-    // std::cout << "Instruction: 0x" << std::hex << (int)instruction << std::endl;
-    // cpu.execute(instruction);
-
-
 }
-
-
